9763d853b1d078423f7ac319f38750c6a7d797d2
RyanOtt <51638926+RyanOttINH@users.noreply.github.com>
2021-01-19 15:36:48 +0100

    Updated strategybot and created dynamic load file

---
+++ bots/stratbotp/load.py
@@ -0,0 +1,55 @@
+"""
+Load file for the fuzzy value of a move
+"""
+from api import State, util
+
+from .kb import Integer
+from .fuzzykb import fuzzyKB, FuzzySymbol
+
+MAX_CARD_POINTS = 11
+
+def general_information(kb, state, index):
+    RV = FuzzySymbol("rv", set_fuzzyRankValue(state, util.get_rank(index)))  # rank value
+    TV = FuzzySymbol("tv", 0.01 if state.get_trump_suit() == util.get_suit(index) else 1)  # trump value
+    SV = FuzzySymbol("sv", set_fuzzySuitValue(state, util.get_suit(index)))  # suit value
+    
+    kb.add_clause(RV)
+    kb.add_clause(TV)
+    kb.add_clause(SV)
+    print(f"RV: {RV.value()}")  # print for debugging
+    print(f"TV: {TV.value()}")
+    print(f"SV: {SV.value()}")
+
+def set_fuzzyRankValue(state, own_rank):
+    points = 1
+    if own_rank == 'J':
+        points = 2
+    elif own_rank == 'Q':
+        points = 3
+    elif own_rank == 'K':
+        points = 4
+    elif own_rank == '10':
+        points = 10
+    else:
+        points = 11
+    return points / MAX_CARD_POINTS
+
+def set_fuzzySuitValue(state, own_suit):
+    clubs_count = 0
+    diamonds_count = 0
+    hearts_count = 0
+    spades_count = 0
+    
+    for card in state.hand():
+        if card < 5 : clubs_count += 1
+        elif card < 10 : diamonds_count += 1
+        elif card < 15 : hearts_count += 1
+        else : spades_count += 1
+    
+    own_count = 0
+    if own_suit == "clubs" : own_count += clubs_count
+    elif own_suit == "diamonds" : own_count += diamonds_count
+    elif own_suit == "hearts" : own_count += hearts_count
+    else : own_count += spades_count
+    
+    return own_count / 5
--- bots/stratbotp/stratbotp.py
+++ bots/stratbotp/stratbotp.py
@@ -7,25 +7,45 @@ It loads general information about the game, as well as the definition of a stra
 from load.py.
 """
 
-import random
 import importlib
+import random
 
 from api import Deck, State, util
 
-# from . import load
 from .kb import KB, Boolean, Integer
+from .fuzzykb import fuzzyKB
+
+from . import load as loadfile
 
 class Bot:
 
     def __init__(self):
         pass
 
+    def card_fuzzyValue(self, state, index):  # returns fuzzy value of given card index
+        kb = fuzzyKB()
+
+        loadfile.general_information(kb, state, index)
+        return kb.fuzzyvalue()
+    
+    def fuzzy_move(self, state, other_moves):  # returns move with highest fuzzy value
+        current_move = other_moves[0]
+        highest_fuzzyValue = 0
+        for move in other_moves:
+            move_fuzzyValue = self.card_fuzzyValue(state, move[0])
+            print(f"FuzzyValue of move {util.get_card_name(move[0])} = {move_fuzzyValue}")
+            if move_fuzzyValue > highest_fuzzyValue:
+                highest_fuzzyValue = move_fuzzyValue
+                current_move = move
+        print(f"Selected highest: {highest_fuzzyValue}")
+        return current_move
+
     def get_move(self, state):
 
         moves = state.moves()
         exchanges = []
         marriages = []
-        trump_moves = []
+        # trump_moves = []
         other_moves = []
 
         random.shuffle(moves)
@@ -35,11 +55,11 @@ class Bot:
                 exchanges.append(move)
             elif move[1] is not None:  # marriage
                 marriages.append(move)
-            elif is_trump(state, move[0]):  # move involving a trump card
-                trump_moves.append(move)
-            # else:
-            #     other_moves.append(move)
-
+            # elif is_trump(state, move[0]):  # move involving a trump card
+                # trump_moves.append(move)
+            else:
+                other_moves.append(move)
+        
         for move in exchanges:
             if not self.kb_consistent(state, move, "trumpex"):  # tells bot to load 'trump exchange' strategy file as kb
                 print("Trump exchange strategy applied")
@@ -50,38 +70,14 @@ class Bot:
                 print("Marriage strategy applied")
                 return move  # Plays the first move that makes the kb inconsistent
         
-        # TEMPORARY taken from bully bot
-        chosen_move = moves[0]
-        # If the opponent has played a card
-        if state.get_opponents_played_card() is not None:
-            moves_same_suit = []
+        return self.fuzzy_move(state, other_moves)  # return the move decided by fuzzy passive strategy
 
-            # Get all moves of the same suit as the opponent's played card
-            for index, move in enumerate(moves):
-                if move[0] is not None and Deck.get_suit(move[0]) == Deck.get_suit(state.get_opponents_played_card()):
-                    moves_same_suit.append(move)
-
-            if len(moves_same_suit) > 0:
-                chosen_move = moves_same_suit[0]
-                return chosen_move
-
-        # Get move with lowest rank available, of any suit
-        for index, move in enumerate(moves):
-            if move[0] is not None and move[0] % 5 >= chosen_move[0] % 5:
-                chosen_move = move
-        return chosen_move
-        # TEMPORARY
-
-        # If no move that is entailed by the kb is found, play random move
-        return random.choice(moves)
-
-    # Note: In this example, the state object is not used,
-    # but you might want to do it for your own strategy.
-    def kb_consistent(self, state, move, strategy):
+    def kb_consistent(self, state, move, strategy):  # checks non-fuzzy KB if move is part of strategy
     # type: (State, move) -> bool
 
         # each time we check for consistency we initialise a new knowledge-base
         kb = KB()
+        # kb = fuzzyKB() if fuzzy_bool else KB()
 
         # load strategy file
         path = f"bots.stratbotp.load_{strategy}"
@@ -103,9 +99,6 @@ class Bot:
         # This creates the string which is used to make the strategy_variable.
         # Note that as far as kb.py is concerned, two objects created with the same
         # string in the constructor are equivalent, and are seen as the same symbol.
-        # Here we use "pj" to indicate that the card with index "index" should be played with the
-        # PlayJack heuristics that was defined in class. Initialise a different variable if 
-        # you want to apply a different strategy (that you will have to define in load.py)
         variable_string = "pc" + str(index)
         strategy_variable = Boolean(variable_string)
 
@@ -115,8 +108,30 @@ class Bot:
         # If the knowledge base is not satisfiable, the strategy variable is
         # entailed (proof by refutation)
         return kb.satisfiable()
-
-def is_trump(state, move):
+    
+def is_trump(state, move):  # not used rn, but could be useful later
     trump_suit = state.get_trump_suit()
     move_suit = util.get_suit(move)
     return trump_suit == move_suit
+
+def temp_move(state, moves):  # not used rn, but if things are not working this is backup
+    chosen_move = moves[0]
+
+    # If the opponent has played a card
+    if state.get_opponents_played_card() is not None:
+        moves_same_suit = []
+
+        # Get all moves of the same suit as the opponent's played card
+        for index, move in enumerate(moves):
+            if move[0] is not None and Deck.get_suit(move[0]) == Deck.get_suit(state.get_opponents_played_card()):
+                moves_same_suit.append(move)
+
+        if len(moves_same_suit) > 0:
+            chosen_move = moves_same_suit[0]
+            return chosen_move
+
+    # Get move with lowest rank available, of any suit
+    for index, move in enumerate(moves):
+        if move[0] is not None and move[0] % 5 >= chosen_move[0] % 5:
+            chosen_move = move
+    return chosen_move
