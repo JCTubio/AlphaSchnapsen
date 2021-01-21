da9bed13e3afcad57416c0b180f750a6e89415f3
RyanOtt <51638926+RyanOttINH@users.noreply.github.com>
2021-01-21 12:56:55 +0100

    Improved is_trump() & implemented phase 1 strategy

--- bots/stratbotp/stratbotp.py
+++ bots/stratbotp/stratbotp.py
@@ -40,12 +40,33 @@ class Bot:
         print(f"Selected highest: {highest_fuzzyValue}")
         return current_move
 
-    def get_move(self, state):
+    def standard_move(self, state, trump_moves, other_moves):
+        if state.get_phase() == 1:
+            if state.whose_turn() == state.leader():
+                return self.fuzzy_move(state, other_moves)
+            else:
+                opponents_card = state.get_opponents_played_card()
+                if is_trump(state, opponents_card):
+                    print("Played lowest ranking move")
+                    return lowest_rank_move(other_moves)
+                elif opponents_card % 5 <= 1:  # if opponent plays ace or 10
+                    if len(trump_moves) > 0:
+                        print("Played lowest owned trump")
+                        return lowest_rank_move(trump_moves)  # win trick using trump to gain many points
+                    else:
+                        print("Played lowest ranking move")
+                        return lowest_rank_move(other_moves)
+                else:  # if opponent plays jack, queen or king
+                    print("Played lowest winning move")
+                    return lowest_winning_move(other_moves, opponents_card)
+        else:  # phase 2
+            return "nope"
 
+    def get_move(self, state):
         moves = state.moves()
         exchanges = []
         marriages = []
-        # trump_moves = []
+        trump_moves = []
         other_moves = []
 
         random.shuffle(moves)
@@ -55,8 +76,8 @@ class Bot:
                 exchanges.append(move)
             elif move[1] is not None:  # marriage
                 marriages.append(move)
-            # elif is_trump(state, move[0]):  # move involving a trump card
-                # trump_moves.append(move)
+            elif is_trump(state, move[0]):  # move involving a trump card
+                trump_moves.append(move)
             else:
                 other_moves.append(move)
         
@@ -70,14 +91,14 @@ class Bot:
                 print("Marriage strategy applied")
                 return move  # Plays the first move that makes the kb inconsistent
         
-        return self.fuzzy_move(state, other_moves)  # return the move decided by fuzzy passive strategy
+        std_move = self.standard_move(state, trump_moves, other_moves)
+        return std_move if std_move != "nope" else self.fuzzy_move(state, other_moves + trump_moves)  # temporary while phase 2 not implemented yet
+        
+        # return self.fuzzy_move(state, other_moves + trump_moves)  # return the move decided by fuzzy passive strategy
 
     def kb_consistent(self, state, move, strategy):  # checks non-fuzzy KB if move is part of strategy
-    # type: (State, move) -> bool
-
         # each time we check for consistency we initialise a new knowledge-base
         kb = KB()
-        # kb = fuzzyKB() if fuzzy_bool else KB()
 
         # load strategy file
         path = f"bots.stratbotp.load_{strategy}"
@@ -85,11 +106,8 @@ class Bot:
             load = importlib.import_module(path)
         except:
             print(f"ERROR: Could not load the python file {path}")
-   
-        # Add general information about the game
-        load.general_information(kb)
 
-        # Add the necessary knowledge about the strategy
+        load.general_information(kb)
         load.strategy_knowledge(kb)
 
         # This line stores the index of the card in the deck.
@@ -109,29 +127,33 @@ class Bot:
         # entailed (proof by refutation)
         return kb.satisfiable()
     
-def is_trump(state, move):  # not used rn, but could be useful later
-    trump_suit = state.get_trump_suit()
-    move_suit = util.get_suit(move)
-    return trump_suit == move_suit
+def is_trump(state, move):
+    return util.get_suit(move) == state.get_trump_suit() if move is not None else "None move"
 
-def temp_move(state, moves):  # not used rn, but if things are not working this is backup
-    chosen_move = moves[0]
+def lowest_rank_move(moves):
+    lowest_move = moves[0]
+    for move in moves:
+        if lowest_move[0] % 5 <= move[0] % 5:
+            lowest_move = move
+    return lowest_move
 
-    # If the opponent has played a card
-    if state.get_opponents_played_card() is not None:
-        moves_same_suit = []
+def lowest_winning_move(moves, opponents_card):
+    """Returns the lowest ranking card that wins current trick against the card the opponent has played.
+    If not possible, returns the lowest ranking card in hand.
 
-        # Get all moves of the same suit as the opponent's played card
-        for index, move in enumerate(moves):
-            if move[0] is not None and Deck.get_suit(move[0]) == Deck.get_suit(state.get_opponents_played_card()):
-                moves_same_suit.append(move)
+    Args:
+        moves (list): List of moves to choose from
+        opponents_card (int): Index of card that opponent has played
 
-        if len(moves_same_suit) > 0:
-            chosen_move = moves_same_suit[0]
-            return chosen_move
+    Returns:
+        tuple: Schnapsen move
+    """
+    higher_same_suit = []  # possible moves that are higher rank and same suit as opponent's card
+    for move in moves:
+        if util.get_suit(move[0]) == util.get_suit(opponents_card) and move[0] % 5 < opponents_card % 5:
+            higher_same_suit.append(move)
 
-    # Get move with lowest rank available, of any suit
-    for index, move in enumerate(moves):
-        if move[0] is not None and move[0] % 5 >= chosen_move[0] % 5:
-            chosen_move = move
-    return chosen_move
+    if len(higher_same_suit) > 0:
+        return lowest_rank_move(higher_same_suit)
+    else:
+        return lowest_rank_move(moves)
